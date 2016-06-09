2fcf2312f531ae2a0ccb2ba3e504a795ae98eca9
emgeorgge <emgeorgge@mycompany.com>
2016-06-09 13:58:31 +0000

    fixing the CORS filter
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/geoserver@12717 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/web/app/src/main/java/org/geoserver/ChorsFilter.java
+++ src/web/app/src/main/java/org/geoserver/ChorsFilter.java
@@ -8,7 +8,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.HttpHeaders;
 
 public class ChorsFilter implements Filter {
 
@@ -20,10 +22,28 @@ public class ChorsFilter implements Filter {
 		ServletRequest request, ServletResponse response, 
 		FilterChain chain) throws IOException, ServletException {
 
+		String method = ((HttpServletRequest)request).getMethod();
+/*
 		((HttpServletResponse)response).addHeader(
-			"Access-Control-Allow-Origin", "*"
+			"Access-Control-Allow-Origin", "http://localhost:9001"
 		);
-		chain.doFilter(request, response);
+		((HttpServletResponse)response).addHeader(
+				"Access-Control-Allow-Credentials", "true"
+		);
+		((HttpServletResponse)response).addHeader(
+				"Access-Control-Allow-Methods", method
+		);
+		((HttpServletResponse)response).addHeader(
+				"Access-Control-Request-Headers", HttpHeaders.AUTHORIZATION
+		);*/
+		HttpServletResponse resp = (HttpServletResponse) response;
+		resp.setHeader("Access-Control-Allow-Origin", "http://localhost:9001");
+		resp.setHeader("Access-Control-Allow-Credentials", "true");
+		resp.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
+		resp.setHeader("Access-Control-Allow-Headers", ((HttpServletRequest)request).getHeader("Access-Control-Request-Headers"));
+		if (!"OPTIONS".equals(method)) {
+			chain.doFilter(request, response);
+		}
 	}
 
 }
